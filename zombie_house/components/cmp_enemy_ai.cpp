18de4991eb1db170a85d113aac75927d3356d3c5
IshidaKuro <lewiswood27@googlemail.com>
2019-04-15 18:10:11 +0100

    updated enemy AI

--- zombie_house/components/cmp_enemy_ai.cpp
+++ zombie_house/components/cmp_enemy_ai.cpp
@@ -4,36 +4,46 @@
 using namespace std;
 using namespace sf;
 
-bool pathing;
+bool pathing = false;
 int random;
 void EnemyAIComponent::update(double dt) {
 
 	
-	//check if the player is close to the zombie
-	if (auto pl = _player.lock()) {
-		if (length(pl->getPosition() - _parent->getPosition()) < 100.0 && pathing == false)
-		{
-			//if they are, check if the zombie should path towards them
-
-			//generate a random number between 1 and 200
-			random = rand() % 199 + 1;
-			//if the result is 1, then path towards the player
-			if (random == 1)
+		//check if the player is close to the zombie
+		if (auto pl = _player.lock()) {
+			
+			if (length(pl->getPosition() - _parent->getPosition()) > 200.0) //if the player is outside the zombie's view
 			{
-				//path towards the player
-				pathing == true;
-				
-				//find which direction to move
-				if (_parent->getPosition().x - pl->getPosition().x < 0)
-					_direction = Vector2f(1.0f, 0);
-				else
-					_direction = Vector2f(-1.0f, 0);
-				
+				_direction = Vector2f(0, 0); // make the zombie stand still
+				pathing = false; // set pathing to false again
+			}
+			
+			else
+			{
+				if (!pathing)
+				{
+					
+	
+					//generate a random number between 1 and 200
+					random = rand() % 199 + 1;
+					//if the result is 1, then path towards the player
+					if (random == 1)
+					{
+						//path towards the player
+						pathing == true;
+	
+						//find which direction to move
+						if (_parent->getPosition().x - pl->getPosition().x < 0)
+							_direction = Vector2f(1.0f, 0);
+						else
+							_direction = Vector2f(-1.0f, 0);
+	
+					}
+				}
+
+
 			}
 		}
-
-
-	}
 	//move in that direction 
 
 	move(_direction * (float)(dt * _speed));
