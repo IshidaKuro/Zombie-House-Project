59a385e747863028530b089555c336c4d3546b91
IshidaKuro <lewiswood27@googlemail.com>
2019-04-15 17:24:54 +0100

    Update cmp_enemy_ai.cpp

--- zombie_house/components/cmp_enemy_ai.cpp
+++ zombie_house/components/cmp_enemy_ai.cpp
@@ -11,18 +11,18 @@ void EnemyAIComponent::update(double dt) {
 	
 	//check if the player is close to the zombie
 	if (auto pl = _player.lock()) {
-		if (length(pl->getPosition() - _parent->getPosition()) < 1000.0 && pathing == false)
+		if (length(pl->getPosition() - _parent->getPosition()) < 100.0 && pathing == false)
 		{
 			//if they are, check if the zombie should path towards them
 
-			//generate a random number between 1 and 20
-			random = 1;
+			//generate a random number between 1 and 200
+			random = rand() % 199 + 1;
 			//if the result is 1, then path towards the player
 			if (random == 1)
 			{
 				//path towards the player
 				pathing == true;
-				cout << "pathing\n";
+				
 				//find which direction to move
 				if (_parent->getPosition().x - pl->getPosition().x < 0)
 					_direction = Vector2f(1.0f, 0);
@@ -43,7 +43,7 @@ void EnemyAIComponent::update(double dt) {
 EnemyAIComponent::EnemyAIComponent(Entity* p) : ActorMovementComponent(p), _player(_parent->scene->ents.find("player")[0])
 {
 	_direction = Vector2f(0, 0);
-	_speed = 100.0f;
+	_speed = 70.0f;
 
 	
 }
