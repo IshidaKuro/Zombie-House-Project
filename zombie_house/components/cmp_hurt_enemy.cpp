77d62a55a52dccc519c8f45048b3a288242ff5bf
Thomas Wood <40295704@live.napier.ac.uk>
2019-04-22 01:38:41 +0100

    All sound effects added

--- zombie_house/components/cmp_hurt_enemy.cpp
+++ zombie_house/components/cmp_hurt_enemy.cpp
@@ -1,9 +1,14 @@
 #include "cmp_hurt_enemy.h"
 #include "cmp_hp.h"
 #include <engine.h>
+#include <SFML/Audio/Sound.hpp>
+#include <SFML/Audio/SoundBuffer.hpp>
 
 using namespace std;
 using namespace sf;
+
+sf::SoundBuffer buffer3;
+sf::Sound sound3;
 
 void HurtEnemyComponent::update(double dt) {
 	//cout << "HP : " << hp;
@@ -12,12 +17,40 @@ void HurtEnemyComponent::update(double dt) {
   {
 	  if (length(pl->getPosition() - _parent->getPosition()) < 25.0) 
 	  {
+		  //play sound
+		  if (!buffer3.loadFromFile("res/sounds/zombie_hurt.wav"))
+		  {
+			  cout << "Error loading zombie sound";
+		  }
+		  else
+		  {
+
+			  cout << "Play sound";
+
+			sound3.setBuffer(buffer3);
+			sound3.play();
+			  
+		  }
+
 		  setHP(getHP() - 1);
 		  if (getHP() <= 0)
 		  {
 			  pl->setForDelete();
 			  // _parent->setForDelete();
 			  cout << "Kill enemy";
+			  if (!buffer3.loadFromFile("res/sounds/zombie_dead.wav"))
+			  {
+				  cout << "Error loading zombie sound";
+			  }
+			  else
+			  {
+				  
+				  cout << "Play sound";
+
+					  sound3.setBuffer(buffer3);
+					  sound3.play();
+				  
+			  }
 		  }
 		  _parent->setForDelete();
 		  //cout << "HP: " << hp;
